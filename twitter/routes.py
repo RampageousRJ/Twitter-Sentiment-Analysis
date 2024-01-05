258e1f355029fa2c68d2f2f617dc3b96bcc54204
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2024-01-05 08:28:17 +0530

    Prototype Completion

--- twitter/routes.py
+++ twitter/routes.py
@@ -2,8 +2,22 @@ from flask import render_template,request
 from twitter import app
 from twitter.forms import TweetForm
 import pickle
+import warnings
+warnings.filterwarnings("ignore")
 
 @app.route('/',methods=['GET','POST'])
 def home():
     form  = TweetForm()
+    predicted_value=''
+    if request.method == 'POST':
+        mfile = open('model/model_pickle','rb')
+        vfile = open('model/vectorizer_pickle','rb')
+        model = pickle.load(mfile)
+        vectorizer = pickle.load(vfile)
+        vector = vectorizer.transform([form.tweet.data])
+        form.tweet.data = ""
+        predicted_value = model.predict(vector)[0]
+        mfile.close()
+        vfile.close()
+        return render_template('home.html',form=form,predicted_value=predicted_value)
     return render_template('home.html',form=form)
